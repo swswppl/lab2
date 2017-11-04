aba209413f0c1ef10b39f5e1ff80ff41aca5370f
swswppl <sws@wp.pl>
2017-11-04 13:35:30 +0100

    first commit

---
+++ src/lab2/klasa.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lab2;
+
+/**
+ * @author dell
+ *
+ */
+public class klasa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Polecana inwestycja:  ");
+
+	}
+
+}
